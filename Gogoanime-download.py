598d885c44a75b44204f19da45f618d4eb6a9379
Marcel Hasse <marcelhasse@localhost.localdomain>
2019-03-26 10:27:42 +0100

    Link fixed

--- Gogoanime-download.py
+++ Gogoanime-download.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import requests
 from pySmartDL import SmartDL
 import sys
@@ -34,7 +36,7 @@ for i in range(int(f),int(l)+1):
             l1=s1.find_all('a')
             for l2 in l1:
                 l3=l2.get('href')
-                if 'fbcdn' in l3:
+                if 'cdnfile' in l3:
                     print(l3)
                     des=d+'/'+n+str(i)+'.mp4'
                     print (des)
